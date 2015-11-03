1b1711ee134a02116f6f6bbd3f7fcaa73cf2d20f
Kevin Huck <khuck@cs.uoregon.edu>
2015-11-02 16:48:45 -0800

    Adding static build for the runtime.

--- dothis.sh
+++ dothis.sh
@@ -1,10 +1,11 @@
 #!/bin/bash -e
 
 cd runtime
+rm -rf build
 mkdir build
 cd build
 
-cmake -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ -DCMAKE_INSTALL_PREFIX=. ..
+cmake -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ -DCMAKE_INSTALL_PREFIX=`pwd`/../../install ..
 
 make
 make install
