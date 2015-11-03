2faff1d87b64291d73b234332bc92c151fb8e4cc
Kevin Huck <khuck@cs.uoregon.edu>
2015-11-03 10:31:50 -0800

    Fixing static build dependencies, and GCC version dependencies.

--- dothis.sh
+++ dothis.sh
@@ -1,12 +1,18 @@
 #!/bin/bash -e
 
-cd runtime
-rm -rf build
-mkdir build
-cd build
-
-cmake -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ -DCMAKE_INSTALL_PREFIX=`pwd`/../../install ..
-
-make
+rm -rf build-gcc
+mkdir build-gcc
+cd build-gcc
+cmake -DCMAKE_C_COMPILER=gcc -DCMAKE_CXX_COMPILER=g++ -DCMAKE_INSTALL_PREFIX=. ..
+make VERBOSE=1
 make install
-cd ../..
+cd ..
+
+rm -rf build-icpc
+mkdir build-icpc
+cd build-icpc
+cmake -DCMAKE_C_COMPILER=icc -DCMAKE_CXX_COMPILER=icpc -DCMAKE_INSTALL_PREFIX=. ..
+make VERBOSE=1
+make install
+cd ..
+
